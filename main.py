dd64d097a9638f0ea900bee9d8efab0697eed2ea
sarvind1119 <69563525+sarvind1119@users.noreply.github.com>
2024-05-24 16:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#importing all the necessary libraries
 import openai
 import langchain
 import pinecone
@@ -10,51 +10,52 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 import os
-
+#function to read the documents
 def read_doc(directory):
     file_loader=PyPDFDirectoryLoader(directory)
     documents=file_loader.load()
     return documents
-
+#mention the directory where the documents are kept
 doc=read_doc('Docs/')
 #len(doc)
-
+#function to make chunks of the documents uploaded
 def chunk_data(docs,chunk_size=800,chunk_overlap=50):
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
     doc=text_splitter.split_documents(docs)
     return docs
-
+#storing the chunks in an object documents
 documents=chunk_data(docs=doc)
 #len(documents)
 
 ## Embedding Technique Of OPENAI
 embeddings=OpenAIEmbeddings(api_key=os.environ['OPENAI_API_KEY'])
 
-# vectors=embeddings.embed_query("How are you?")
-# len(vectors)
-
 from langchain_pinecone import PineconeVectorStore
 
+#uploading the embeddings into Pinecone vectordatabase
 vectorstore_from_docs = PineconeVectorStore.from_documents(
     documents,
     index_name='renewable1',
     embedding=embeddings
 )
 
-## Cosine Similarity Retreive Results from VectorDB
-def retrieve_query(query,k=2):
-    matching_results=vectorstore_from_docs.similarity_search(query,k=k)
-    return matching_results
 
-from langchain.chains.question_answering import load_qa_chain
-from langchain import OpenAI
+#following code is for q&a without making a UI. Uncomment and run if you want to check else ignore and go to the file app.py
 
-llm=OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0.5)
-chain=load_qa_chain(llm,chain_type="stuff")
+# ## Cosine Similarity Retreive Results from VectorDB
+# def retrieve_query(query,k=2):
+#     matching_results=vectorstore_from_docs.similarity_search(query,k=k)
+#     return matching_results
 
-## Search answers from VectorDB
-def retrieve_answers(query):
-    doc_search=retrieve_query(query)
-    print(doc_search)
-    response=chain.run(input_documents=doc_search,question=query)
-    return response
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain import OpenAI
+
+# llm=OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0.5)
+# chain=load_qa_chain(llm,chain_type="stuff")
+
+# ## Search answers from VectorDB
+# def retrieve_answers(query):
+#     doc_search=retrieve_query(query)
+#     print(doc_search)
+#     response=chain.run(input_documents=doc_search,question=query)
+#     return response
